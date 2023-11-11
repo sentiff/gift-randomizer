89aa2e83efcb972ae280aa277953df8460fc551a
sentiff <66693737+sentiff@users.noreply.github.com>
2023-11-11 14:38:56 +0100

    moved to dir

--- giftRandomizerApp/src/main/java/org/gift/randomizer/app/controllers/HealthController.java
+++ giftRandomizerApp/src/main/java/org/gift/randomizer/app/controllers/HealthController.java
@@ -1,4 +1,4 @@
-package org.gift.randomizer.app;
+package org.gift.randomizer.app.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
