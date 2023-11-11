7b95db3d71b0c4dc1d2abf874733b2948a15a33c
sentiff <66693737+sentiff@users.noreply.github.com>
2023-11-11 23:39:04 +0100

    refactored to use responseUtils

--- giftRandomizerApp/src/main/java/org/gift/randomizer/app/controllers/ParticipantController.java
+++ giftRandomizerApp/src/main/java/org/gift/randomizer/app/controllers/ParticipantController.java
@@ -5,6 +5,7 @@ import lombok.val;
 import org.gift.randomizer.app.db.NoCandidateException;
 import org.gift.randomizer.app.db.InMemoryDB;
 import org.gift.randomizer.app.utils.JsonUtils;
+import org.gift.randomizer.app.utils.ResponseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,19 +24,22 @@ public class ParticipantController {
     private JsonUtils jsonUtils;
 
     @Autowired
+    private ResponseUtils responseUtils;
+
+    @Autowired
     private InMemoryDB inMemoryDB;
 
     @GetMapping("/getParticipantByName")
     public ResponseEntity<String> getParticipantByName(@RequestParam(value = "name") String name) {
         try {
             val person = inMemoryDB.getParticipantByName(name);
-            return createResponse(
+            return responseUtils.createResponse(
                     jsonUtils.toJson(person),
                     APPLICATION_JSON,
                     HttpStatus.OK
             );
         } catch (NoCandidateException | JsonProcessingException e) {
-            return createResponse(
+            return responseUtils.createResponse(
                     e.getMessage(),
                     TEXT_PLAIN,
                     HttpStatus.BAD_GATEWAY
@@ -47,13 +51,13 @@ public class ParticipantController {
     public ResponseEntity<String> getParticipantById(@RequestParam(value = "id", defaultValue = "1") Long id) {
         try {
             val person = inMemoryDB.getParticipantById(id);
-            return createResponse(
+            return responseUtils.createResponse(
                     jsonUtils.toJson(person),
                     APPLICATION_JSON,
                     HttpStatus.OK
             );
         } catch (NoCandidateException | JsonProcessingException e) {
-            return createResponse(
+            return responseUtils.createResponse(
                     e.getMessage(),
                     TEXT_PLAIN,
                     HttpStatus.BAD_GATEWAY
@@ -65,13 +69,13 @@ public class ParticipantController {
     public ResponseEntity<String> addParticipant(@RequestParam(value = "name") String name, @RequestParam(value = "giftIdeas") List<String> giftIdeas) {
         try {
             val dbResponse = inMemoryDB.addParticipant(name, giftIdeas);
-            return createResponse(
+            return responseUtils.createResponse(
                     jsonUtils.toJson(dbResponse),
                     APPLICATION_JSON,
                     HttpStatus.OK
             );
         } catch (Exception e) {
-            return createResponse(
+            return responseUtils.createResponse(
                     e.getMessage(),
                     TEXT_PLAIN,
                     HttpStatus.BAD_GATEWAY
@@ -83,13 +87,13 @@ public class ParticipantController {
     public ResponseEntity<String> updateParticipantById(@RequestParam(value = "id") Long id, @RequestParam(value = "name") String name, @RequestParam(value = "gift ideas") List<String> rawGiftIdeas) {
         try {
             val dbResponse = inMemoryDB.updateParticipantById(id, name, rawGiftIdeas);
-            return createResponse(
+            return responseUtils.createResponse(
                     jsonUtils.toJson(dbResponse),
                     APPLICATION_JSON,
                     HttpStatus.OK
             );
         } catch (Exception e) {
-            return createResponse(
+            return responseUtils.createResponse(
                     e.getMessage(),
                     TEXT_PLAIN,
                     HttpStatus.BAD_GATEWAY
@@ -101,13 +105,13 @@ public class ParticipantController {
     public ResponseEntity<String> updateParticipantById(@RequestParam(value = "id") Long id, @RequestParam(value = "name") String name) {
         try {
             val dbResponse = inMemoryDB.updateParticipantById(id, name);
-            return createResponse(
+            return responseUtils.createResponse(
                     jsonUtils.toJson(dbResponse),
                     APPLICATION_JSON,
                     HttpStatus.OK
             );
         } catch (Exception e) {
-            return createResponse(
+            return responseUtils.createResponse(
                     e.getMessage(),
                     TEXT_PLAIN,
                     HttpStatus.BAD_GATEWAY
@@ -119,13 +123,13 @@ public class ParticipantController {
     public ResponseEntity<String> updateParticipantById(@RequestParam(value = "id") Long id, @RequestParam(value = "gift ideas") List<String> rawGiftIdeas) {
         try {
             val dbResponse = inMemoryDB.updateParticipantById(id, rawGiftIdeas);
-            return createResponse(
+            return responseUtils.createResponse(
                     jsonUtils.toJson(dbResponse),
                     APPLICATION_JSON,
                     HttpStatus.OK
             );
         } catch (Exception e) {
-            return createResponse(
+            return responseUtils.createResponse(
                     e.getMessage(),
                     TEXT_PLAIN,
                     HttpStatus.BAD_GATEWAY
@@ -137,31 +141,17 @@ public class ParticipantController {
     public ResponseEntity<String> removeParticipantById(@RequestParam(value = "id") Long id) {
         try {
             val dbResponse = inMemoryDB.removeParticipantById(id);
-            return createResponse(
+            return responseUtils.createResponse(
                     jsonUtils.toJson(dbResponse),
                     APPLICATION_JSON,
                     HttpStatus.OK
             );
         } catch (Exception e) {
-            return createResponse(
+            return responseUtils.createResponse(
                     e.getMessage(),
                     TEXT_PLAIN,
                     HttpStatus.BAD_GATEWAY
             );
         }
-    }
-
-    private LinkedMultiValueMap<String, String> createHeaders(String contentType) {
-        val responseHeaders = new LinkedMultiValueMap<String, String>();
-        responseHeaders.add("Content-Type", contentType);
-        return responseHeaders;
-    }
-
-    private ResponseEntity<String> createResponse(String body, String contentType, HttpStatus status) {
-        return new ResponseEntity<>(
-                body,
-                createHeaders(contentType),
-                status
-        );
     }
 }
