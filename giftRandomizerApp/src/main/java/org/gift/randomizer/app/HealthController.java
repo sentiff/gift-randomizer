047a32478fb9a6e709b15e32ab1ad031c9e531ae
sentiff <66693737+sentiff@users.noreply.github.com>
2023-11-11 13:07:53 +0100

    added health controller

---
+++ giftRandomizerApp/src/main/java/org/gift/randomizer/app/HealthController.java
@@ -0,0 +1,13 @@
+package org.gift.randomizer.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String getHealthStatus() {
+        return "OK";
+    }
+}
