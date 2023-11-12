9a46b34a04d2b78aa46e8fabe9bc653c33fecc83
sentiff <66693737+sentiff@users.noreply.github.com>
2023-11-12 12:56:02 +0100

    removed unused import

--- giftRandomizerApp/src/main/java/org/gift/randomizer/app/controllers/ParticipantController.java
+++ giftRandomizerApp/src/main/java/org/gift/randomizer/app/controllers/ParticipantController.java
@@ -9,7 +9,6 @@ import org.gift.randomizer.app.utils.ResponseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
