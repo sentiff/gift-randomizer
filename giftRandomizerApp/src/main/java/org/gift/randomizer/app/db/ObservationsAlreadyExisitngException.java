041d5af72acbd2a19ebc4a93a04fbcc9fee69e5e
sentiff <66693737+sentiff@users.noreply.github.com>
2023-11-12 16:03:55 +0100

    added observation exception

---
+++ giftRandomizerApp/src/main/java/org/gift/randomizer/app/db/ObservationsAlreadyExisitngException.java
@@ -0,0 +1,7 @@
+package org.gift.randomizer.app.db;
+
+public class ObservationsAlreadyExisitngException extends Exception{
+    public ObservationsAlreadyExisitngException(String message) {
+        super(message);
+    }
+}
