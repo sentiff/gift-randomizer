04fa43e36ba604e9df8bf5e6686eef278b17abb5
sentiff <66693737+sentiff@users.noreply.github.com>
2023-11-13 08:31:55 +0100

    renamed exception

--- giftRandomizerApp/src/main/java/org/gift/randomizer/app/db/ObservationsAlreadyExisitngException.java
+++
@@ -1,7 +0,0 @@
-package org.gift.randomizer.app.db;
-
-public class ObservationsAlreadyExisitngException extends Exception{
-    public ObservationsAlreadyExisitngException(String message) {
-        super(message);
-    }
-}
