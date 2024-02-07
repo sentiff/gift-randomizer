c0850eedaac9661173dd1d7a61e1461289c7c593
sentiff <66693737+sentiff@users.noreply.github.com>
2024-02-07 22:57:46 +0100

    moved model and InMemoryDB to commons module

--- commons/src/main/java/org/sentiff/gift/randomizer/commons/db/model/exceptions/ObservationsException.java
+++ commons/src/main/java/org/sentiff/gift/randomizer/commons/db/model/exceptions/ObservationsException.java
@@ -1,4 +1,4 @@
-package org.sentiff.gift.randomizer.db.model.exceptions;
+package org.sentiff.gift.randomizer.commons.db.model.exceptions;
 
 public class ObservationsException extends Exception{
     public ObservationsException(String message) {
--- commons/src/main/java/org/sentiff/gift/randomizer/commons/db/model/exceptions/ParticipantException.java
+++ commons/src/main/java/org/sentiff/gift/randomizer/commons/db/model/exceptions/ParticipantException.java
@@ -1,4 +1,4 @@
-package org.sentiff.gift.randomizer.db.model.exceptions;
+package org.sentiff.gift.randomizer.commons.db.model.exceptions;
 
 public class ParticipantException extends Exception{
     public ParticipantException(String msg) {
