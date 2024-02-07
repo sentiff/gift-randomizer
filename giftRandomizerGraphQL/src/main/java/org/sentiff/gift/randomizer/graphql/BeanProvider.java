6df11fc11a1d3e1e76f60091c5baccb22701fc7d
sentiff <66693737+sentiff@users.noreply.github.com>
2024-02-07 23:03:37 +0100

    added bean provider

---
+++ giftRandomizerGraphQL/src/main/java/org/sentiff/gift/randomizer/graphql/BeanProvider.java
@@ -0,0 +1,37 @@
+package org.sentiff.gift.randomizer.graphql;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.sentiff.gift.randomizer.commons.db.InMemoryDB;
+import org.sentiff.gift.randomizer.commons.db.model.GiftIdea;
+import org.sentiff.gift.randomizer.commons.db.model.Observation;
+import org.sentiff.gift.randomizer.commons.db.model.Participant;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+@Configuration
+public class BeanProvider {
+
+    @Bean
+    public OpenAPI apiDocConfig() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("gift randomizer graphQL")
+                        .version("0.0.1-SNAPSHOT"));
+    }
+
+    @Bean
+    public InMemoryDB getInMemoryDB() {
+        final LinkedList<Participant> participants = new LinkedList<>();
+        participants.add(new Participant(1L, "Janusz", List.of(new GiftIdea("passerati"))));
+        participants.add(new Participant(2L, "Grażynka", List.of(new GiftIdea("djament"))));
+        participants.add(new Participant(3L, "Pjoter", List.of(new GiftIdea("ajfon"))));
+        final LinkedList<Observation> observations = new LinkedList<>();
+        final Random randomGenerator = new Random();
+        return new InMemoryDB(participants, observations, randomGenerator);
+    }
+}
