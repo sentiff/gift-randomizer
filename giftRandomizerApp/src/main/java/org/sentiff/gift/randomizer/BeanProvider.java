4d1e01fde7c6a91f9147841e5b52bb600f7db9ab
sentiff <66693737+sentiff@users.noreply.github.com>
2025-10-21 21:27:54 +0200

    incremented version in apiDocs, fixed storage import

--- giftRandomizerApp/src/main/java/org/sentiff/gift/randomizer/BeanProvider.java
+++ giftRandomizerApp/src/main/java/org/sentiff/gift/randomizer/BeanProvider.java
@@ -2,7 +2,7 @@ package org.sentiff.gift.randomizer;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.sentiff.gift.randomizer.commons.Storage;
+import org.sentiff.gift.randomizer.commons.storage.Storage;
 import org.sentiff.gift.randomizer.commons.model.GiftIdea;
 import org.sentiff.gift.randomizer.commons.model.Observation;
 import org.sentiff.gift.randomizer.commons.model.Participant;
@@ -24,7 +24,7 @@ public class BeanProvider {
         return new OpenAPI()
                 .info(new Info()
                         .title("gift randomizer API")
-                        .version("0.0.4"));
+                        .version("0.1.0"));
     }
 
     @Bean
